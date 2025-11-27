b8f7bf00bb8f083d62cff317ea14660d33d83e86
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-11-27 15:44:21 +0100

    feat: ajout du chargement automatique des variables d'environnement avec Dotenv (.env)

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 require_once __DIR__ . '/../core/config.php';
 require_once __DIR__ . '/../core/helpers.php';
 
